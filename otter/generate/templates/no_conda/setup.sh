c0ed5691c5cfc84dc98d4ee30dbe687f58cf303e
tinsirius <ttra0024@student.monash.edu>
2022-11-08 13:48:31 +1100

    print for debugging

--- otter/generate/templates/no_conda/setup.sh
+++ otter/generate/templates/no_conda/setup.sh
@@ -4,7 +4,7 @@ if [ "${BASE_IMAGE}" != "ucbdsinfra/otter-grader" ]; then
     apt-get clean
     apt-get update
     apt-get install -y texlive-base pandoc texlive-xetex texlive-fonts-recommended build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libgit2-dev texlive-lang-chinese
-
+    sed -i '144i \    print(tests_run)' /usr/local/lib/python3.8/dist-packages/otter/execute/__init__.py
     # install wkhtmltopdf
     wget --quiet -O /tmp/wkhtmltopdf.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
     apt-get install -y /tmp/wkhtmltopdf.deb
